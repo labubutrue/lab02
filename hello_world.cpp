8ae91579a846e1fcf859f8dd11bfb170b541bfdd
labubutrue <chimichanga2123837@gmail.com>
2026-09-03 01:03:54 +0300

    Fix code style, remove using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
-int main(){
-string name;
-cin>>name;
-cout<<"Hello world from "<<name<<endl;
-return 0;
+int main()
+{
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
+    return 0;
 }
