db44d5b2ab6cdf253fd56c5ba8a7b2bfc752f9e1
labubutrue <chimichanga2123837@gmail.com>
2026-09-03 01:31:24 +0300

    Apply clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,11 +4,12 @@
 // Эта программа для Hello World с персонализацией
 // Удален using namespace std
 
-int main()
+int
+main()
 {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
